46167c2ef5ecd5d94fe1bd292a83780fd836926e
Paulb23 <p_batty@hotmail.co.uk>
2015-10-10 18:38:11 +0100

    fields in metadata

--- src/main/java/battyp/lancaster/sqlitevisualiser/model/datastructures/Metadata.java
+++ src/main/java/battyp/lancaster/sqlitevisualiser/model/datastructures/Metadata.java
@@ -30,4 +30,116 @@ package battyp.lancaster.sqlitevisualiser.model.datastructures;
  * @author Paul Batty
  */
 public class Metadata {
+
+    /**
+     * The database Page size in bytes
+     */
+    public int pageSize;
+
+    /**
+     * Format write version, 1 for legacy; 2 for WAL
+     */
+    public int writeVersion;
+
+    /**
+     * Format read version, 1 for legacy; 2 for WAL
+     */
+    public int readVersion;
+
+    /**
+     * Free Space or unused space at the end of each page
+     */
+    public int unusedSpaceAtEndOfEachPage;
+
+    /**
+     * Maximum Embedded payload Must be 64
+     */
+    public int maxEmbeddedPayload;
+
+    /**
+     * Minimum Embedded payload Must be 32
+     */
+    public int minEmbeddedPayload;
+
+    /**
+     * Leaf payload Fraction Must be 32
+     */
+    public int leafPayloadFraction;
+
+    /**
+     * The File Change counter
+     */
+    public int fileChageCounter;
+
+    /**
+     * Size of the database file in pages
+     */
+    public int sizeOfDatabaseInPages;
+
+
+    /**
+     * Page number of first freelist page
+     */
+    public int pageNumberOfFirstFreelistPage;
+
+    /**
+     * The total number of freelist pages
+     */
+    public int totalFreeListPages;
+
+    /**
+     * The schema cookie
+     */
+    public int schemaCookie;
+
+    /**
+     * The schema format valid formats are, 1,2,3,4
+     */
+    public int schemaFormat;
+
+    /**
+     * The default page cache size
+     */
+    public int defualtPageCacheSize;
+
+    /**
+     * Page number to the largest Btree else zero
+     */
+    public int pageNumberToLargestBTreePage;
+
+    /**
+     * The type of text encoding the database uses,
+     * 1 = UTF-8
+     * 2 = UTF-16le
+     * 3 = UTF-16be
+     */
+    public int textEncoding;
+
+    /**
+     * The User version set by user_version_pragma
+     */
+    public int userVersion;
+
+
+    /**
+     * The vacuum mode
+     * 1 = incremental-vacuum
+     * 2 = auto-vacuum
+     */
+    public int vacuummMode;
+
+    /**
+     * The application id
+     */
+    public int appID;
+
+    /**
+     * The version valid number
+     */
+    public int versionValidNumber;
+
+    /**
+     * The sqlite version
+     */
+    public int sqliteVersion;
 }
