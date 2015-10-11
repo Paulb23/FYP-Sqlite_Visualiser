4b0ac00263b9c3cc5e03410600642b97b04d1369
Paulb23 <p_batty@hotmail.co.uk>
2015-10-11 01:41:27 +0100

    Database Interface Class

---
+++ src/main/java/battyp/lancaster/sqlitevisualiser/model/databaseinterface/DefaultDatabaseInterface.java
@@ -0,0 +1,112 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Paul Batty
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package battyp.lancaster.sqlitevisualiser.model.databaseinterface;
+
+import battyp.lancaster.sqlitevisualiser.model.database.Database;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * DefaultDatabaseInterface is a database interface for the databases objects
+ *
+ * @see battyp.lancaster.sqlitevisualiser.model.database.Database
+ *
+ * @author Paul Batty
+ */
+public class DefaultDatabaseInterface implements DatabaseInferface {
+
+    private int current;
+    private List<Database> history;
+
+    /**
+     * Creates a new DatabaseInterface
+     */
+    public DefaultDatabaseInterface() {
+        this.current = 0;
+        this.history = new ArrayList<>();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Database getCurrent() {
+        if (this.current < this.history.size() && this.current >= 0) {
+            return this.history.get(this.current);
+        }
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Database getPrevious() {
+        if (this.current - 1 < this.history.size() && this.current - 1 >= 0) {
+            return this.history.get(this.current - 1);
+        }
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Database getNext() {
+        if (this.current + 1 < this.history.size() && this.current + 1 >= 0) {
+            return this.history.get(this.current + 1);
+        }
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void nextStep() {
+        if (this.current < this.history.size() - 1) {
+            this.current++;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void previousStep() {
+        if (this.current < this.history.size() + 1 && this.current != 0) {
+            this.current--;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void addDatabase(Database database) {
+        this.history.add(database);
+    }
+}
---
+++ src/test/java/battyp/lancaster/sqlitevisualiser/tests/model/databaseinterface/DefaultDatabaseInterfaceTest.java
@@ -0,0 +1,156 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Paul Batty
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package battyp.lancaster.sqlitevisualiser.tests.model.databaseinterface;
+
+import battyp.lancaster.sqlitevisualiser.model.database.Database;
+import battyp.lancaster.sqlitevisualiser.model.databaseinterface.DefaultDatabaseInterface;
+import battyp.lancaster.sqlitevisualiser.model.datastructures.BTree;
+import battyp.lancaster.sqlitevisualiser.model.datastructures.Metadata;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * JUnit tests for DefaultDatabaseInterface
+ *
+ * @see battyp.lancaster.sqlitevisualiser.model.databaseinterface.DefaultDatabaseInterface
+ *
+ * @author Paul Batty
+ */
+public class DefaultDatabaseInterfaceTest {
+
+    @Test
+    public void TestGetCurrentReturnsNullOnCreation() {
+        DefaultDatabaseInterface databaseInterface = new DefaultDatabaseInterface();
+        Assert.assertEquals(null, databaseInterface.getCurrent());
+    }
+
+    @Test
+    public void TestGetPreviousReturnsNullOnCreation() {
+        DefaultDatabaseInterface databaseInterface = new DefaultDatabaseInterface();
+        Assert.assertEquals(null, databaseInterface.getPrevious());
+    }
+
+    @Test
+    public void TestGetNextReturnsNullOnCreation() {
+        DefaultDatabaseInterface databaseInterface = new DefaultDatabaseInterface();
+        Assert.assertEquals(null, databaseInterface.getNext());
+    }
+
+    @Test
+    public void TestNextStepDoesNothingOnCreation() {
+        DefaultDatabaseInterface databaseInterface = new DefaultDatabaseInterface();
+        databaseInterface.nextStep();
+        Assert.assertEquals(null, databaseInterface.getCurrent());
+    }
+
+    @Test
+    public void TestPreviousStepDoesNothingOnCreation() {
+        DefaultDatabaseInterface databaseInterface = new DefaultDatabaseInterface();
+        databaseInterface.previousStep();
+        Assert.assertEquals(null, databaseInterface.getCurrent());
+    }
+
+    @Test
+    public void TestAddAndGetCurrentReturnsCorrectDatabase() {
+        DefaultDatabaseInterface databaseInterface = new DefaultDatabaseInterface();
+        Database database = new Database(new BTree<String>(), new Metadata());
+        databaseInterface.addDatabase(database);
+        Assert.assertEquals(database, databaseInterface.getCurrent());
+    }
+
+    @Test
+    public void TestAddAndGetNextReturnsNull() {
+        DefaultDatabaseInterface databaseInterface = new DefaultDatabaseInterface();
+        Database database = new Database(new BTree<String>(), new Metadata());
+        databaseInterface.addDatabase(database);
+        Assert.assertEquals(null, databaseInterface.getNext());
+    }
+
+    @Test
+    public void TestAddAndGetPreviousReturnsNull() {
+        DefaultDatabaseInterface databaseInterface = new DefaultDatabaseInterface();
+        Database database = new Database(new BTree<String>(), new Metadata());
+        databaseInterface.addDatabase(database);
+        Assert.assertEquals(null, databaseInterface.getPrevious());
+    }
+
+    @Test
+    public void TestAddAndNextStepDoesNothing() {
+        DefaultDatabaseInterface databaseInterface = new DefaultDatabaseInterface();
+        Database database = new Database(new BTree<String>(), new Metadata());
+        databaseInterface.addDatabase(database);
+        databaseInterface.nextStep();
+        Assert.assertEquals(database, databaseInterface.getCurrent());
+    }
+
+    @Test
+    public void TestAddAndPreviousStepDoesNothing() {
+        DefaultDatabaseInterface databaseInterface = new DefaultDatabaseInterface();
+        Database database = new Database(new BTree<String>(), new Metadata());
+        databaseInterface.addDatabase(database);
+        databaseInterface.previousStep();
+        Assert.assertEquals(database, databaseInterface.getCurrent());
+    }
+
+    @Test
+    public void TestNextStep() {
+        DefaultDatabaseInterface databaseInterface = new DefaultDatabaseInterface();
+        Database database = new Database(new BTree<String>(), new Metadata());
+        databaseInterface.addDatabase(new Database(new BTree<String>(), new Metadata()));
+        databaseInterface.addDatabase(database);
+        databaseInterface.nextStep();
+        Assert.assertEquals(database, databaseInterface.getCurrent());
+    }
+
+    @Test
+    public void TestPreviousStep() {
+        DefaultDatabaseInterface databaseInterface = new DefaultDatabaseInterface();
+        Database database = new Database(new BTree<String>(), new Metadata());
+        databaseInterface.addDatabase(database);
+        databaseInterface.addDatabase(new Database(new BTree<String>(), new Metadata()));
+        databaseInterface.nextStep();
+        databaseInterface.previousStep();
+        Assert.assertEquals(database, databaseInterface.getCurrent());
+    }
+
+    @Test
+    public void TestGetNext() {
+        DefaultDatabaseInterface databaseInterface = new DefaultDatabaseInterface();
+        Database database = new Database(new BTree<String>(), new Metadata());
+        databaseInterface.addDatabase(new Database(new BTree<String>(), new Metadata()));
+        databaseInterface.addDatabase(database);
+        Assert.assertEquals(database, databaseInterface.getNext());
+    }
+
+    @Test
+    public void TestGetPrevious() {
+        DefaultDatabaseInterface databaseInterface = new DefaultDatabaseInterface();
+        Database database = new Database(new BTree<String>(), new Metadata());
+        databaseInterface.addDatabase(database);
+        databaseInterface.addDatabase(new Database(new BTree<String>(), new Metadata()));
+        databaseInterface.nextStep();
+        Assert.assertEquals(database, databaseInterface.getPrevious());
+    }
+}
